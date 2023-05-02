5bad4bb7fb2a3a03c19aeb85100401c009b2e178
Matyáš Novotný <novotny.matyas@ssakhk.cz>
2023-05-02 12:29:26 +0200

    initial

---
+++ Emergency_Medical_Service/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Emergency_Medical_Service.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
