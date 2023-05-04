9ce5ad6659c3734eb1a4372c17d91ea2fd1893a2
Matyáš Novotný <novotny.matyas@ssakhk.cz>
2023-05-04 13:34:48 +0200

    migration

--- EMS.Lib/Models/DoctorModel.cs
+++ EMS.Lib/Models/DoctorModel.cs
@@ -6,6 +6,8 @@ public class DoctorModel
     public int HospitalId { get; set; }
     public string Name { get; set; }
     public string Surname { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
     public DateOnly Birthday { get; set; }
     public string PhoneNumber { get; set; }
     public string CallSign { get; set; }
--- Emergency_Medical_Service/Migrations/20230504113113_pw.Designer.cs
+++ Emergency_Medical_Service/Migrations/20230504113113_pw.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Emergency_Medical_Service.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Emergency_Medical_Service.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230504113113_pw")]
+    partial class pw
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
