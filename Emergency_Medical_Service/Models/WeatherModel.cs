3f7009bb31968157f407cada1d365e4a4fe4419c
Matyáš Novotný <novotny.matyas@ssakhk.cz>
2023-05-23 15:16:43 +0200

    last edit?

---
+++ Emergency_Medical_Service/Models/WeatherModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Emergency_Medical_Service.Models;
+
+public class WeatherModel
+{
+    // Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+    public class Forecast
+    {
+        public int Day { get; set; }
+        public string Temperature { get; set; }
+        public string Wind { get; set; }
+    }
+
+    public class Root
+    {
+        public string Temperature { get; set; }
+        public string Wind { get; set; }
+        public string Description { get; set; }
+        public List<Forecast> Forecast { get; set; }
+    }
+}
---
+++ Emergency_Medical_Service/Services/WeatherAPIService.cs
@@ -0,0 +1,34 @@
+﻿using Emergency_Medical_Service.Models;
+
+namespace Emergency_Medical_Service.Services;
+
+public class WeatherAPIService
+{
+    private readonly HttpClient _client;
+    private readonly ILogger<WeatherAPIService> _logger;
+
+    public WeatherAPIService(ILogger<WeatherAPIService> logger)
+    {
+        _client = new();
+
+        _logger = logger;
+    }
+
+    private const string API_V1 = "https://goweather.herokuapp.com/weather/";
+
+    public async Task<WeatherModel> Weather(string city)
+    {
+        city = "Hradec";
+
+        try
+        {
+            return await _client.GetFromJsonAsync<WeatherModel>($"{API_V1}{city}");
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"ERROR: " + ex.Message);
+        }
+
+        return default;
+    }
+}
